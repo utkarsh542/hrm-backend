00b21d4e2f6432547d3e8deec8031514a13e9876
utkarsh542 <gutkarsh191@gmail.com>
2026-05-28 13:12:29 +0530

    add logger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """HRMS Backend — FastAPI Application Entry Point."""
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
